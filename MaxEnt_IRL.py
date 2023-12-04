fdf93b700763982ede2a32865fc7c64a33c74832
wangyuchen0217 <wangyuchen0217@outlook.com>
2023-12-04 11:45:24 +0900

    commit

--- MaxEnt_IRL.py
+++ MaxEnt_IRL.py
@@ -1,36 +1,31 @@
 import os
-import numpy as np
 import gym
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.optimize import minimize
 
 class MaxEntIRL:
-    def __init__(self, expert_data, state_dim, action_dim, gamma=0.9, learning_rate=0.01, epochs=100):
+    def __init__(self, expert_data, state_dim, learning_rate=0.01, epochs=100):
         self.expert_data = expert_data
         self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.gamma = gamma
         self.learning_rate = learning_rate
         self.epochs = epochs
-
         # Initialize the reward weights randomly
         self.weights = np.random.rand(state_dim)
 
     def compute_feature_expectations(self, trajectories):
         total_feature_expectations = np.zeros(self.state_dim)
-
         for trajectory in trajectories:
             for state in trajectory:
                 total_feature_expectations += state
-
         return total_feature_expectations / len(trajectories)
 
     def compute_state_action_visitation(self, trajectories):
         state_action_count = np.zeros(self.state_dim)
-
         for trajectory in trajectories:
             for state in trajectory:
                 state_action_count += state
-
         return state_action_count / len(trajectories)
 
     def compute_softmax_policy(self, weights, state):
@@ -47,41 +42,46 @@ class MaxEntIRL:
         for epoch in range(self.epochs):
             # Compute state-action visitation frequency using the current policy
             state_action_visitation = self.compute_state_action_visitation(self.expert_data)
-
             # Compute feature expectations from the expert data
             expert_feature_expectations = self.compute_feature_expectations(self.expert_data)
-
             # Compute policy using the current reward weights
             policy = np.array([self.compute_softmax_policy(self.weights, state) for state in state_action_visitation])
-
             # Compute expected feature counts under the current policy
             expected_feature_counts = self.compute_expected_feature_counts(policy, state_action_visitation)
-
             # Compute the gradient of the reward function
             gradient = self.compute_gradient(expert_feature_expectations, expected_feature_counts)
-
             # Update the reward weights
             self.weights += self.learning_rate * gradient.mean(axis=0)
-
             # Print progress
             if epoch % 10 == 0:
                 print(f"Epoch {epoch}, Reward Weights: {self.weights}")
-
         return self.weights
+    
+    def plot_training_progress(self):
+        plt.plot(range(self.epochs), [np.linalg.norm(weights) for weights in self.reward_history])
+        plt.xlabel("Epoch")
+        plt.ylabel("Norm of Reward Weights")
+        plt.title("Training Progress")
+        plt.show()
+        savefig("training_progress.png")   
 
 # Example usage
 if __name__ == "__main__":
     fold_path = os.getcwd()
-    expert_data = np.random.rand(100, 12)
+    data_path = os.path.join(fold_path, "Expert_data_builder","demo_dataset.csv")
+    expert_data  = pd.read_csv(data_path, header=[0], index_col=[0]).to_numpy()
 
     # Define the state and action dimensions
     state_dim = 12
-    action_dim = 12
+    epochs = 100
 
     # Create MaxEntIRL object
-    maxent_irl = MaxEntIRL(expert_data, state_dim, action_dim)
-
+    maxent_irl = MaxEntIRL(expert_data, state_dim, epochs)
     # Run MaxEnt IRL
     learned_weights = maxent_irl.maxent_irl()
-
+    # Print the learned reward weights
     print("Learned Reward Weights:", learned_weights)
+    # Save the learned reward weights
+    np.save("learned_weights.npy", learned_weights)
+    # Plot the training progress
+    maxent_irl.plot_training_progress()

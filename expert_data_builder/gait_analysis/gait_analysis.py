c541002ea25e91cf3752b3321ddaf319fb5262d6
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-01-31 16:32:03 +0900

    commit

--- expert_data_builder/gait_analysis/gait_analysis.py
+++ expert_data_builder/gait_analysis/gait_analysis.py
@@ -2,6 +2,85 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from pykalman import KalmanFilter
+
+def Kalman1D(observations,damping=1):
+    observation_covariance = damping
+    initial_value_guess = observations[0]
+    transition_matrix = 1
+    transition_covariance = 0.03
+    initial_value_guess
+    kf = KalmanFilter(
+            initial_state_mean=initial_value_guess,
+            initial_state_covariance=observation_covariance,
+            observation_covariance=observation_covariance,
+            transition_covariance=transition_covariance,
+            transition_matrices=transition_matrix
+            )
+    pred_state, state_cov = kf.smooth(observations)
+    return pred_state
+
+def data_smooth(data):
+    for i in range(data.shape[1]):
+        smoothed_data = Kalman1D(data[:,i], damping=1).reshape(-1,1)
+        data[:,i] = smoothed_data[:,0]
+    return data
+
+def peak_detection(data):
+    # data: 1D array
+    peak_indices = []
+    for i in range(1, len(data)-1):
+        if data[i] > data[i-1] and data[i] >= data[i+1]:
+            peak_indices.append(i)
+    return peak_indices
+
+def valley_detection(data):
+    # data: 1D array
+    valley_indices = []
+    for i in range(1, len(data)-1):
+        if data[i] < data[i-1] and data[i] <= data[i+1]:
+            valley_indices.append(i)
+    return valley_indices
+
+def plot_gait_phase(data):
+    peak_indices = peak_detection(data)
+    valley_indices = valley_detection(data)
+    if peak_indices[0] < valley_indices[0]:
+        # add the first data point as a valley
+        valley_indices.insert(0, 0)
+    else:
+        # add the first data point as a peak
+        peak_indices.insert(0, 0)
+    if peak_indices[-1] < valley_indices[-1]:
+        # add the last data point as a peak
+        peak_indices.append(len(data)-1)
+    else:
+        # add the last data point as a valley
+        valley_indices.append(len(data)-1)
+
+    t = np.arange(len(data))
+    plt.plot(t, data)
+    # draw vertical lines at peak indices to show the gait cycle
+    for i in peak_indices:
+        plt.axvline(x=i, color='grey')
+    # stance phase: peak to valley, green
+    # swing phase: valley to peak, orange
+    if peak_indices[0] < valley_indices[0]:
+        for i in range(len(peak_indices)-1):
+            plt.axvspan(peak_indices[i], valley_indices[i], facecolor='g', alpha=0.3)
+            plt.axvspan(valley_indices[i], peak_indices[i+1], facecolor='orange', alpha=0.3)
+        if peak_indices[-1] < valley_indices[-1]:
+            plt.axvspan(peak_indices[-1], valley_indices[-1], facecolor='g', alpha=0.3)
+    else:
+        for i in range(len(valley_indices)-1):
+            plt.axvspan(valley_indices[i], peak_indices[i], facecolor='orange', alpha=0.3)
+            plt.axvspan(peak_indices[i], valley_indices[i+1], facecolor='g', alpha=0.3)
+        if peak_indices[-1] > valley_indices[-1]:
+            plt.axvspan(valley_indices[-1], peak_indices[-1], facecolor='orange', alpha=0.3)
+    # plt.xlabel('Frame')
+    # plt.ylabel('Joint Angle')
+    # plt.title('Gait Phase')
+    #plt.show()
 
 # temperary test for c21-0680 data
 fold_path = os.getcwd() + '/expert_data_builder'
@@ -9,35 +88,17 @@ cricket_number = 'c21'
 video_number = '0680'
 joint_path = os.path.join(fold_path, 'joint_movement', cricket_number, f'PIC{video_number}_Joint_movement.csv')
 joint_movement = pd.read_csv(joint_path, header=[0], index_col=[0])
+joint_movement = joint_movement.values
+joint_movement = data_smooth(joint_movement)
+# plot_gait_phase(joint_movement[:,1])
+# plt.show()
 
-def plot_gait_phase_diagram(time, joint_angles):
-    # Define the threshold angle to distinguish between stance and swing phase
-    threshold_angle = 0  # You may need to adjust this based on your data
+# subplot for all joints
+plt.figure(figsize=(20,8))
+for i in range(joint_movement.shape[1]):
+    plt.subplot(6,2,i+1)
+    plot_gait_phase(joint_movement[:,i])
+plt.tight_layout()
+plt.subplots_adjust(hspace=0.07)
+plt.show()
 
-    # Identify stance and swing phases based on the threshold angle
-    stance_phase = joint_angles > threshold_angle
-    swing_phase = joint_angles <= threshold_angle
-
-    # Plot the joint angles
-    plt.plot(time, joint_angles, label='Joint Angles')
-
-    # Highlight stance and swing phases
-    plt.fill_between(time, joint_angles, where=stance_phase, color='green', alpha=0.3, label='Stance Phase')
-    plt.fill_between(time, joint_angles, where=swing_phase, color='orange', alpha=0.3, label='Swing Phase')
-
-    # Add labels and legend
-    plt.xlabel('Time')
-    plt.ylabel('Joint Angles')
-    plt.title('Gait Phase Diagram')
-    plt.legend()
-
-    # Show the plot
-    plt.show()
-
-# Example usage
-# Replace these with your actual time and joint angle data
-time_data = np.linspace(0, 10, 100)
-joint_angle_data = np.sin(time_data)  # Replace this with your actual joint angle data
-
-# Call the function to plot the gait phase diagram
-plot_gait_phase_diagram(time_data, joint_angle_data)

5dc88fa3894494cb0045b780b8a4a64c091848e1
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-03-02 12:52:53 +0900

    damage csv revision

--- expert_data_builder/joint_movement_analysis.py
+++ expert_data_builder/joint_movement_analysis.py
@@ -13,7 +13,7 @@ def get_joint_movement(subject:str, fold_path):
         trail_details = json.load(f)
         cricket_number =  trail_details[f"T{subject}"]["cricket_number"]
         video_number = trail_details[f"T{subject}"]["video_number"]
-    joint_path = fold_path + '/joint_movement/' + cricket_number + '/PIC' + video_number + '_joint_movement.csv'
+    joint_path = fold_path + '/joint_movement/' + cricket_number + '/PIC' + video_number + '_Joint_movement.csv'
     joint_movement = pd.read_csv(joint_path, header=[0], index_col=[0])
     df_joint = pd.DataFrame(data=joint_movement)
     return df_joint
@@ -33,13 +33,16 @@ for i in range(subjects):
         trail_details = json.load(f)
         cricket_number =  trail_details[f"T{subject_number}"]["cricket_number"]
         video_number = trail_details[f"T{subject_number}"]["video_number"]
-    # get the max and min of each joint_movement
-    df_joint = get_joint_movement(subject_number, fold_path)
-    min = df_joint.min()
-    max = df_joint.max()
-    # add the max and min to the joint_range
-    joint_range.loc[cricket_number+'_'+video_number+'_'+'min'] = min
-    joint_range.loc[cricket_number+'_'+video_number+'_'+'max'] = max
+    if cricket_number=="c16"or cricket_number=="c18"or cricket_number=="c20":
+        continue
+    else:
+        # get the max and min of each joint_movement
+        df_joint = get_joint_movement(subject_number, fold_path)
+        min = df_joint.min()
+        max = df_joint.max()
+        # add the max and min to the joint_range
+        joint_range.loc[cricket_number+'_'+video_number+'_'+'min'] = min
+        joint_range.loc[cricket_number+'_'+video_number+'_'+'max'] = max  
 
 joint_min = joint_range.min()
 joint_max = joint_range.max()

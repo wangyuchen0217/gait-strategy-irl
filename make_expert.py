3be089b01654330783e6ad7698e5efdb85f13fec
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-03-04 17:31:36 +0900

    commit

--- make_expert.py
+++ make_expert.py
@@ -81,42 +81,26 @@ viewer = mujoco_py.MjViewer(sim)
 # np.save("CricketEnv2D-v0.npy", trajectories)
 
 '''firl-2d moving position'''
-def trajectory_calculation(vel):
-    # vel: [vel.x, vel.y]
-    # scale velocity: mm/s
-    vel = vel * 0.224077
-    # frequency: 119.88(120)Hz
-    # trajectory calculation
-    traj_x = [0]; x=0
-    traj_y = [0]; y=0
-    for i in range(1, len(vel)):
-        x = x + vel[i][0]*1/119.88/100 # dm
-        y = y + vel[i][1]*1/119.88/100 # dm
-        traj_x.append(x)
-        traj_y.append(y)
-    traj = np.array([traj_x, traj_y]).reshape(-1, 2)
-    return traj
-
 cricket_number = 'c21'
 video_number = '0680'
 joint_path = os.path.join("expert_data_builder/movement", cricket_number, 
                                                 f"PIC{video_number}_Joint_movement.csv")
 direction_path = os.path.join("expert_data_builder/movement", cricket_number,
                                                 f"PIC{video_number}_Heading_direction.csv")
-vel_path = os.path.join("expert_data_builder/velocity_data", cricket_number, 
-                                                f"{video_number}_Velocity_Smooth.csv")
+traj_path = os.path.join("expert_data_builder/movement", cricket_number,
+                                                f"PIC{video_number}_Trajectory.csv")
 joint_movement = pd.read_csv(joint_path, header=[0], index_col=[0]).to_numpy()
 heading_direction = pd.read_csv(direction_path, header=[0], index_col=[0]).to_numpy()
-vel = pd.read_csv(vel_path, header=None, usecols=[1,2]).to_numpy() # vel.x and vel.y
-traj = trajectory_calculation(vel)
+traj = pd.read_csv(traj_path, header=[0], index_col=[0]).to_numpy() # traj.x and traj.y
+# traj scale
+traj = traj * 100 # original measurement is in meters
 trajecroty = []
 for i in range(7100): # 7100 is the length of each trajectory
     joint_angle = np.deg2rad(joint_movement[i])
     direction = np.deg2rad(heading_direction[i])
-    #sim.data.ctrl[:12] = joint_angle
-    #sim.data.ctrl[12] = traj[i, 0]
-    sim.data.ctrl[13] = traj[i, 1]
-    #sim.data.ctrl[14] = direction
+    sim.data.ctrl[:12] = joint_angle
+    sim.data.ctrl[12:14] = traj[i, :]
+    sim.data.ctrl[14] = direction
     sim.step()
     viewer.render()
     state = np.hstack((sim.get_state().qpos[:12].copy(), 

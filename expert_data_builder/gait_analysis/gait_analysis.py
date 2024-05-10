e3fee88abb375594d72b51676f1e6490d26bf96c
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-05-10 12:21:10 +0900

    copmmit

--- expert_data_builder/gait_analysis/gait_analysis.py
+++ expert_data_builder/gait_analysis/gait_analysis.py
@@ -152,9 +152,9 @@ for i in range(6):
     gait_phase_FTi[:,i] = gait_generate(joint_movement[:,i+6], reverse=reverse_list_FTi[i])
 gait_phase = np.concatenate((gait_phase_ThC, gait_phase_FTi), axis=1)
 save_path = os.path.join(fold_path, 'movement', cricket_number, f'PIC{video_number}_Gait_phase.csv')
-pd.DataFrame(gait_phase).to_csv(save_path, 
-                                header=["ThC_LF","ThC_LM","ThC_LH","ThC_RF","ThC_RM","ThC_RH", 
-                                        "FTi_LF","FTi_LM","FTi_LH","FTi_RF","FTi_RM", "FTi_RH"], index=True)
+# pd.DataFrame(gait_phase).to_csv(save_path, 
+#                                 header=["ThC_LF","ThC_LM","ThC_LH","ThC_RF","ThC_RM","ThC_RH", 
+#                                         "FTi_LF","FTi_LM","FTi_LH","FTi_RF","FTi_RM", "FTi_RH"], index=True)
 
 # subplot for ThC joints
 joint_movement = joint_movement[:200,:]

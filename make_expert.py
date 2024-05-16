a92256febccc0515e208828752ce86205641597e
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-05-16 12:16:06 +0900

    commit

--- make_expert.py
+++ make_expert.py
@@ -140,7 +140,7 @@ plt.ylabel("y")
 plt.title("c21_0680_trajectory_simulated")
 plt.grid()
 # plt.show()
-plt.savefig("c21_0680_trajectory_simulated.png")
+plt.savefig("c21_0680_trajectory_simulated_test.png")
 
 '''firl-3d  ThC joint smoothed data position'''
 # cricket_number = 'c21'

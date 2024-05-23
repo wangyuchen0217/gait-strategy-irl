9aff6c2716e16e78c7f0c9798cc0f42fea6d7e05
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-05-23 17:53:17 +0900

    commit

---
+++ stick_insect/python.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+data_path = 'data.csv'
+dataset = pd.read_csv(data_path, header=[0], index_col=None).to_numpy()
+print(dataset.shape)
+
+fem = dataset[:, :6]
+cox_y = dataset[:, 6:]
+print(fem.shape, cox_y.shape)
+
+lev_dep = fem + cox_y
+print(lev_dep.shape)
+
+save_path = 'lev_dep.csv'
+pd.DataFrame(lev_dep).to_csv(save_path, header=None, index=None)

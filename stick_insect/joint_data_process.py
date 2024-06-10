d6c92734aa6f087387285d00deb480d8e9fc31ba
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-06-10 12:13:06 +0900

    commit

--- stick_insect/joint_data_process.py
+++ stick_insect/joint_data_process.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-data_path = 'data.csv'
+data_path = 'sup_process_source.csv'
 dataset = pd.read_csv(data_path, header=[0], index_col=None).to_numpy()
 print(dataset.shape)
 
@@ -14,5 +14,5 @@ print(fem.shape, cox_y.shape)
 lev_dep = fem + cox_y
 print(lev_dep.shape)
 
-save_path = 'lev_dep.csv'
+save_path = 'sup_data.csv'
 pd.DataFrame(lev_dep).to_csv(save_path, header=None, index=None)

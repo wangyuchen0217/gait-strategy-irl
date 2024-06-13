38e1579e47d8a69b9e8197683e077e3e13495db3
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-06-13 14:06:29 +0900

    commit

--- envs/StickInsectEnv.py
+++ envs/StickInsectEnv.py
@@ -1,88 +1,200 @@
 import numpy as np
-import gymnasium as gym
+
 from gymnasium import utils
-from gymnasium.envs.mujoco import MuJocoPyEnv
+from gymnasium.envs.mujoco import MujocoEnv
 from gymnasium.spaces import Box
 
-class StickInsectEnv(MuJocoPyEnv, utils.EzPickle):
+
+DEFAULT_CAMERA_CONFIG = {
+    "distance": 4.0,
+}
+
+
+class StickInsectEnv(MujocoEnv, utils.EzPickle):
     metadata = {
         "render_modes": [
             "human",
             "rgb_array",
             "depth_array",
         ],
-        "render_fps":  100,
+        "render_fps": 100,
     }
 
-    def __init__(self, **kwargs):
+    def __init__(
+        self,
+        xml_file="/home/yuchen/insect_walking_irl/envs/assets/StickInsect-v0.xml",
+        ctrl_cost_weight=0.5,
+        use_contact_forces=False,
+        contact_cost_weight=5e-4,
+        healthy_reward=1.0,
+        terminate_when_unhealthy=True,
+        healthy_z_range=(0.2, 1.0),
+        contact_force_range=(-1.0, 1.0),
+        reset_noise_scale=0.1,
+        exclude_current_positions_from_observation=True,
+        **kwargs,
+    ):
+        utils.EzPickle.__init__(
+            self,
+            xml_file,
+            ctrl_cost_weight,
+            use_contact_forces,
+            contact_cost_weight,
+            healthy_reward,
+            terminate_when_unhealthy,
+            healthy_z_range,
+            contact_force_range,
+            reset_noise_scale,
+            exclude_current_positions_from_observation,
+            **kwargs,
+        )
+
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._contact_cost_weight = contact_cost_weight
+
+        self._healthy_reward = healthy_reward
+        self._terminate_when_unhealthy = terminate_when_unhealthy
+        self._healthy_z_range = healthy_z_range
+
+        self._contact_force_range = contact_force_range
+
+        self._reset_noise_scale = reset_noise_scale
+
+        self._use_contact_forces = use_contact_forces
+
+        self._exclude_current_positions_from_observation = (
+            exclude_current_positions_from_observation
+        )
+
+        obs_shape = 27
+        if not exclude_current_positions_from_observation:
+            obs_shape += 2
+        if use_contact_forces:
+            obs_shape += 84
+
         observation_space = Box(
-            low=np.concatenate((np.full(7, -np.inf), np.full(24, -np.pi), np.full(6, -np.inf), np.full(24, -np.inf))),
-            high=np.concatenate((np.full(7, np.inf), np.full(24, np.pi), np.full(6, np.inf), np.full(24, np.inf))),
-            dtype=np.float64
+            low=-np.inf, high=np.inf, shape=(obs_shape,), dtype=np.float64
         )
-        MuJocoPyEnv.__init__(
-            self, "/home/yuchen/insect_walking_irl/envs/assets/StickInsect-v1.xml", 2, observation_space=observation_space, **kwargs
-        )
-        utils.EzPickle.__init__(self, **kwargs)
 
-    def step(self, a):
-        xposbefore = self.get_body_com("torso")[0]
-        self.do_simulation(a, self.frame_skip)
-        xposafter = self.get_body_com("torso")[0]
-
-        forward_reward = (xposafter - xposbefore) / self.dt
-        ctrl_cost = 0.5 * np.square(a).sum()
-        contact_cost = (
-            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        MujocoEnv.__init__(
+            self,
+            xml_file,
+            2,
+            observation_space=observation_space,
+            default_camera_config=DEFAULT_CAMERA_CONFIG,
+            **kwargs,
         )
-        survive_reward = 1.0
-        reward = forward_reward - ctrl_cost - contact_cost + survive_reward
+
+    @property
+    def healthy_reward(self):
+        return (
+            float(self.is_healthy or self._terminate_when_unhealthy)
+            * self._healthy_reward
+        )
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
+        return control_cost
+
+    @property
+    def contact_forces(self):
+        raw_contact_forces = self.data.cfrc_ext
+        min_value, max_value = self._contact_force_range
+        contact_forces = np.clip(raw_contact_forces, min_value, max_value)
+        return contact_forces
+
+    @property
+    def contact_cost(self):
+        contact_cost = self._contact_cost_weight * np.sum(
+            np.square(self.contact_forces)
+        )
+        return contact_cost
+
+    @property
+    def is_healthy(self):
         state = self.state_vector()
-        not_terminated = (
-            np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
-        )
-        terminated = not not_terminated
-        ob = self._get_obs()
+        min_z, max_z = self._healthy_z_range
+        is_healthy = np.isfinite(state).all() and min_z <= state[2] <= max_z
+        return is_healthy
+
+    @property
+    def terminated(self):
+        terminated = not self.is_healthy if self._terminate_when_unhealthy else False
+        return terminated
+
+    def step(self, action):
+        xy_position_before = self.get_body_com("torso")[:2].copy()
+        self.do_simulation(action, self.frame_skip)
+        xy_position_after = self.get_body_com("torso")[:2].copy()
+
+        xy_velocity = (xy_position_after - xy_position_before) / self.dt
+        x_velocity, y_velocity = xy_velocity
+
+        forward_reward = x_velocity
+        healthy_reward = self.healthy_reward
+
+        rewards = forward_reward + healthy_reward
+
+        costs = ctrl_cost = self.control_cost(action)
+
+        terminated = self.terminated
+        observation = self._get_obs()
+        info = {
+            "reward_forward": forward_reward,
+            "reward_ctrl": -ctrl_cost,
+            "reward_survive": healthy_reward,
+            "x_position": xy_position_after[0],
+            "y_position": xy_position_after[1],
+            "distance_from_origin": np.linalg.norm(xy_position_after, ord=2),
+            "x_velocity": x_velocity,
+            "y_velocity": y_velocity,
+            "forward_reward": forward_reward,
+        }
+        if self._use_contact_forces:
+            contact_cost = self.contact_cost
+            costs += contact_cost
+            info["reward_ctrl"] = -contact_cost
+
+        reward = rewards - costs
 
         if self.render_mode == "human":
             self.render()
         # truncation=False as the time limit is handled by the `TimeLimit` wrapper added during `make`
-        return (
-            ob,
-            reward,
-            terminated,
-            False,
-            dict(
-                reward_forward=forward_reward,
-                reward_ctrl=-ctrl_cost,
-                reward_contact=-contact_cost,
-                reward_survive=survive_reward,
-            ),
-        )
+        return observation, reward, terminated, False, info
 
     def _get_obs(self):
-        return np.concatenate(
-            [
-                self.sim.data.qpos.flat[:],
-                self.sim.data.qvel.flat[:]
-            ]
-        )
+        position = self.data.qpos.flat.copy()
+        velocity = self.data.qvel.flat.copy()
+
+        if self._exclude_current_positions_from_observation:
+            position = position[2:]
+
+        if self._use_contact_forces:
+            contact_force = self.contact_forces.flat.copy()
+            return np.concatenate((position, velocity, contact_force))
+        else:
+            return np.concatenate((position, velocity))
 
     def reset_model(self):
-        qpos = self.init_qpos + self.np_random.uniform(
-            size=self.model.nq, low=-0.1, high=0.1
-        )
-        qvel = self.init_qvel + self.np_random.standard_normal(self.model.nv) * 0.1
-        self.set_state(qpos, qvel)
-        return self._get_obs()
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
 
-    def viewer_setup(self):
-        assert self.viewer is not None
-        self.viewer.cam.distance = self.model.stat.extent * 0.5
+        qpos = self.init_qpos + self.np_random.uniform(
+            low=noise_low, high=noise_high, size=self.model.nq
+        )
+        qvel = (
+            self.init_qvel
+            + self._reset_noise_scale * self.np_random.standard_normal(self.model.nv)
+        )
+        self.set_state(qpos, qvel)
+
+        observation = self._get_obs()
+
+        return observation
 
 if __name__ == "__main__":
     env = StickInsectEnv(render_mode='human')
-    env.reset()
+    env.reset_model()
 
     # print the observation space and action space
     print("observation space:", env.observation_space)
---
+++ envs/StickInsectEnv_gymnasium_210.py
@@ -0,0 +1,96 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium import utils
+from gymnasium.envs.mujoco import MuJocoPyEnv
+from gymnasium.spaces import Box
+
+class StickInsectEnv(MuJocoPyEnv, utils.EzPickle):
+    metadata = {
+        "render_modes": [
+            "human",
+            "rgb_array",
+            "depth_array",
+        ],
+        "render_fps":  100,
+    }
+
+    def __init__(self, **kwargs):
+        observation_space = Box(
+            low=np.concatenate((np.full(7, -np.inf), np.full(24, -np.pi), np.full(6, -np.inf), np.full(24, -np.inf))),
+            high=np.concatenate((np.full(7, np.inf), np.full(24, np.pi), np.full(6, np.inf), np.full(24, np.inf))),
+            dtype=np.float64
+        )
+        MuJocoPyEnv.__init__(
+            self, "/home/yuchen/insect_walking_irl/envs/assets/StickInsect-v1.xml", 2, observation_space=observation_space, **kwargs
+        )
+        utils.EzPickle.__init__(self, **kwargs)
+
+    def step(self, a):
+        xposbefore = self.get_body_com("torso")[0]
+        self.do_simulation(a, self.frame_skip)
+        xposafter = self.get_body_com("torso")[0]
+
+        forward_reward = (xposafter - xposbefore) / self.dt
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
+        survive_reward = 1.0
+        reward = forward_reward - ctrl_cost - contact_cost + survive_reward
+        state = self.state_vector()
+        not_terminated = (
+            np.isfinite(state).all() and state[2] >= 0.2 and state[2] <= 1.0
+        )
+        terminated = not not_terminated
+        ob = self._get_obs()
+
+        if self.render_mode == "human":
+            self.render()
+        # truncation=False as the time limit is handled by the `TimeLimit` wrapper added during `make`
+        return (
+            ob,
+            reward,
+            terminated,
+            False,
+            dict(
+                reward_forward=forward_reward,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+            ),
+        )
+
+    def _get_obs(self):
+        return np.concatenate(
+            [
+                self.sim.data.qpos.flat[:],
+                self.sim.data.qvel.flat[:]
+            ]
+        )
+
+    def reset_model(self):
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
+        qvel = self.init_qvel + self.np_random.standard_normal(self.model.nv) * 0.1
+        self.set_state(qpos, qvel)
+        return self._get_obs()
+
+    def viewer_setup(self):
+        assert self.viewer is not None
+        self.viewer.cam.distance = self.model.stat.extent * 0.5
+
+if __name__ == "__main__":
+    env = StickInsectEnv(render_mode='human')
+    env.reset()
+
+    # print the observation space and action space
+    print("observation space:", env.observation_space)
+    print("observation space shape:", env.observation_space.shape)
+    print("action space:", env.action_space)
+    print("action space shape:", env.action_space.shape)
+
+    for _ in range(1000):
+        env.step(env.action_space.sample())
+        env.render()
+    env.close()

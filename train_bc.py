8aac7213f24d84d0e5cee6f7ad8a9ad5b077bb4f
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-06-18 17:01:31 +0900

    commit

--- train_bc.py
+++ train_bc.py
@@ -22,6 +22,7 @@ import envs
 import yaml
 import matplotlib.pyplot as plt
 import logging
+import torch
 
 SEED = 42
 
@@ -35,24 +36,53 @@ env = gym.make('StickInsect-v0',  exclude_current_positions_from_observation=exc
 env = DummyVecEnv([lambda: RolloutInfoWrapper(env)])
 
 # Load the expert dataset
-expert = np.load('expert_demonstration/expert/StickInsect-v0-m3t-12.npy', allow_pickle=True)
+obs_states = np.load('expert_demonstration/expert/StickInsect-v0-m3t-12-obs.npy', allow_pickle=True)
+actions = np.load('expert_demonstration/expert/StickInsect-v0-m3t-12-act.npy', allow_pickle=True)
 
 # Extract observations and "actions" (which are the next observations in this context)
-# Avoid using the last transition because it lacks a valid next observation
-observations = expert[0, :-1, :-48] if exclude_xy else expert[0, :-2, :]
-actions = expert[0, 1:-1, -48:]   # Adjusted to avoid the last transition
-next_observations = expert[0, 2:, 2:] if exclude_xy else expert[0, 2:, :]
+observations = obs_states[0, :-1, 2:] if exclude_xy else obs_states[0, :-1, :] # Exclude the last step to avoid indexing error
+actions = actions[0, :-1, :] 
+
+next_observations = obs_states[0, 1:, 2:] if exclude_xy else obs_states[0, 1:, :] # Exclude the first step to avoid indexing error
 
 dones = np.zeros(len(observations), dtype=bool)
-# dones[-1] = True  # Mark the last timestep as terminal
+dones[-1] = True  # Mark the last timestep as terminal
 
-transitions = {
-    'obs': observations,
-    'acts': actions,
-    'next_obs': next_observations,
-    'dones': np.zeros(len(observations), dtype=bool)
-}
+# transit the data to types.Transitions
+transitions = types.Transitions(
+    obs=observations,
+    acts=actions,
+    next_obs=next_observations,
+    dones=dones,
+    infos=[{} for _ in range(len(observations))]
+)
 
+# expert = PPO(
+#     policy=MlpPolicy,
+#     env=env,
+#     seed=0,
+#     batch_size=64,
+#     ent_coef=0.0,
+#     learning_rate=0.0003,
+#     n_epochs=10,
+#     n_steps=64,
+# )
+
+# reward, _ = evaluate_policy(expert, env, 10)
+# print(f"Reward before training: {reward}")
+
+# expert.learn(10_000)  # Note: set to 100000 to train a proficient expert
+# reward, _ = evaluate_policy(expert, expert.get_env(), 10)
+# print(f"Expert reward: {reward}")
+
+# rng = np.random.default_rng()
+# rollouts = rollout.rollout(
+#     expert,
+#     env,
+#     rollout.make_sample_until(min_timesteps=None, min_episodes=50),
+#     rng=rng,
+# )
+# transitions = rollout.flatten_trajectories(rollouts)
 
 # Create the BC trainer
 bc_trainer = bc.BC(
@@ -68,3 +98,6 @@ print(f"Reward before training: {reward_before_training}")
 bc_trainer.train(n_epochs=1)
 reward_after_training, _ = evaluate_policy(bc_trainer.policy, env, 10)
 print(f"Reward after training: {reward_after_training}")
+
+# save the trained model
+torch.save(bc_trainer, "trained_policy_bc.pth")

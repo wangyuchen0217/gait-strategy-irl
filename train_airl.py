d7facb925f3e8451d8fc1d279b759f7c54c0f3e7
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-07-23 13:46:46 +0900

    commit

--- train_airl.py
+++ train_airl.py
@@ -38,14 +38,19 @@ env = gym.make('StickInsect-v0',
 env = DummyVecEnv([lambda: RolloutInfoWrapper(env)])
 
 # Load the expert dataset
-obs_states = np.load('expert_demonstration/expert/StickInsect-v0-m3t-12-obs.npy', allow_pickle=True)
-actions = np.load('expert_demonstration/expert/StickInsect-v0-m3t-12-act.npy', allow_pickle=True)
+obs_states = np.load('expert_demonstration/expert/StickInsect-32-obs.npy', allow_pickle=True)
+actions = np.load('expert_demonstration/expert/StickInsect-32-act.npy', allow_pickle=True)
 
 # Extract observations and "actions" (which are the next observations in this context)
-observations = obs_states[0, :-1, 2:] if exclude_xy else obs_states[0, :-1, :] # Exclude the last step to avoid indexing error
-actions = actions[0, :-1, :] 
+# observations = obs_states[0, :-1, 2:] if exclude_xy else obs_states[0, :-1, :] # Exclude the last step to avoid indexing error
+# actions = actions[0, :-1, :] 
 
-next_observations = obs_states[0, 1:, 2:] if exclude_xy else obs_states[0, 1:, :] # Exclude the first step to avoid indexing error
+# next_observations = obs_states[0, 1:, 2:] if exclude_xy else obs_states[0, 1:, :] # Exclude the first step to avoid indexing error
+
+observations = obs_states[:-1, 2:] if exclude_xy else obs_states[:-1, :] # Exclude the last step to avoid indexing error
+actions = actions[:-1, :] 
+
+next_observations = obs_states[1:, 2:] if exclude_xy else obs_states[1:, :] # Exclude the first step to avoid indexing error
 
 dones = np.zeros(len(observations), dtype=bool)
 # dones[-1] = True  # Mark the last timestep as terminal

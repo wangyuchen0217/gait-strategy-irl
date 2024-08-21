0c4c08bb27251aa3b397e5192d554692ffc2ada8
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-08-21 14:40:43 +0900

    commit

--- train.py
+++ train.py
@@ -10,9 +10,11 @@ data = pd.read_csv('expert_demonstration/expert/CarausiusC00.csv')
 n_velocity_bins = data['Velocity Bin'].nunique()
 n_direction_bins = data['Direction Bin'].nunique()
 n_gait_categories = data['Gait Category'].nunique()
+print("---------------------------------")
 print("Velocity bins: ", n_velocity_bins)
 print("Direction bins: ", n_direction_bins)
 print("Gait categories: ", n_gait_categories)
+print("---------------------------------")
 
 mdp = MDP(n_velocity_bins, n_direction_bins, n_gait_categories, discount=0.9)
 
@@ -32,12 +34,14 @@ for index, row in data.iterrows():
 # Generate trajectories from the dataset
 trajectories = []
 for index, row in data.iterrows():
-    state_index = int(row['Velocity Bin'] * n_direction_bins + row['Direction Bin'])
+    state_index = int((row['Velocity Bin']-1) * n_direction_bins + (row['Direction Bin']-1))
     action = int(row['Gait Category'])
     trajectories.append([(state_index, action)])
 
 # Set up transition probabilities (for simplicity, we'll assume deterministic transitions here)
 transition_probabilities = np.eye(n_states)[np.newaxis].repeat(mdp.n_actions, axis=0)
+print("Transition probabilities shape: ", transition_probabilities.shape)
+print("---------------------------------")
 
 # Set transition probabilities in MDP
 mdp.set_transition_probabilities(transition_probabilities)

8e2c7642ef523017e37d5b6ed61229cb34a9ceb2
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-11-21 18:02:38 +0900

    commit

---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+q_value_ante = np.loadtxt('test_folder/maxent/Carausius/ante_gait/q_values_maxent_direction.csv', delimiter=',')
+q_value_dirvel = np.loadtxt('test_folder/maxent/Carausius/direct_gait/q_values_maxent_direction.csv', delimiter=',')
+q_value_accvel = np.loadtxt('test_folder/maxent/Carausius/accelerated_gait/q_values_maxent_velocity.csv', delimiter=',')
+

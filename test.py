9b316e32958dd116a3de605e3f31771f8fad0296
Yuhen-W <wangyuchen0217@outlook.com>
2024-11-27 17:10:44 +0900

    commit

--- test.py
+++ test.py
@@ -1,27 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-q_value_ante = np.loadtxt('test_folder/maxent/Carausius/ante_gait/q_values_maxent_direction.csv', delimiter=',')
-q_value_dirvel = np.loadtxt('test_folder/maxent/Carausius/dirvel_gait/q_values_maxent_direction.csv', delimiter=',')
-q_value_accvel = np.loadtxt('test_folder/maxent/Carausius/accvel_gait/q_values_maxent_velocity.csv', delimiter=',')
+q_value_ante_carausius = np.loadtxt('test_folder/maxent/Carausius/ante_gait/q_values_maxent_direction.csv', delimiter=',')
+q_value_dirvel_carausius = np.loadtxt('test_folder/maxent/Carausius/dirvel_gait/q_values_maxent_direction.csv', delimiter=',')
+q_value_accvel_carausius = np.loadtxt('test_folder/maxent/Carausius/accvel_gait/q_values_maxent_velocity.csv', delimiter=',')
+q_value_dirvel_aretaon = np.loadtxt('test_folder/maxent/Aretaon/dirvel_gait/q_values_maxent_direction.csv', delimiter=',')
+q_value_accvel_aretaon = np.loadtxt('test_folder/maxent/Aretaon/accvel_gait/q_values_maxent_velocity.csv', delimiter=',')
+q_value_dirvel_medauroidea = np.loadtxt('test_folder/maxent/Medauroidea/dirvel_gait/q_values_maxent_direction.csv', delimiter=',')
+q_value_accvel_medauroidea = np.loadtxt('test_folder/maxent/Medauroidea/accvel_gait/q_values_maxent_velocity.csv', delimiter=',')
 
-trajectory_ante = np.loadtxt('test_folder/maxent/Carausius/ante_gait/trajectories.csv', delimiter=',')
-trajectory_dirvel = np.loadtxt('test_folder/maxent/Carausius/dirvel_gait/trajectories.csv', delimiter=',')
-trajectory_accvel = np.loadtxt('test_folder/maxent/Carausius/accvel_gait/trajectories.csv', delimiter=',')
+trajectory_ante_carausius = np.loadtxt('test_folder/maxent/Carausius/ante_gait/trajectories.csv', delimiter=',')
+trajectory_dirvel_carausius = np.loadtxt('test_folder/maxent/Carausius/dirvel_gait/trajectories.csv', delimiter=',')
+trajectory_accvel_carausius = np.loadtxt('test_folder/maxent/Carausius/accvel_gait/trajectories.csv', delimiter=',')
+trajectory_dirvel_aretaon = np.loadtxt('test_folder/maxent/Aretaon/dirvel_gait/trajectories.csv', delimiter=',')
+trajectory_accvel_aretaon = np.loadtxt('test_folder/maxent/Aretaon/accvel_gait/trajectories.csv', delimiter=',')
+trajectory_dirvel_medauroidea = np.loadtxt('test_folder/maxent/Medauroidea/dirvel_gait/trajectories.csv', delimiter=',')
+trajectory_accvel_medauroidea = np.loadtxt('test_folder/maxent/Medauroidea/accvel_gait/trajectories.csv', delimiter=',')
 
-state_indices_ante = trajectory_ante[:, 0].astype(int)
-state_indices_dirvel = trajectory_dirvel[:, 0].astype(int)
-state_indices_accvel = trajectory_accvel[:, 0].astype(int)
-actions = trajectory_ante[:, 1].astype(int)
+state_indices_ante_carausius = trajectory_ante_carausius[:, 0].astype(int)
+state_indices_dirvel_carausius = trajectory_dirvel_carausius[:, 0].astype(int)
+state_indices_accvel_carausius = trajectory_accvel_carausius[:, 0].astype(int)
+actions_carausius = trajectory_ante_carausius[:, 1].astype(int)
+state_indices_dirvel_aretaon = trajectory_dirvel_aretaon[:, 0].astype(int)
+state_indices_accvel_aretaon = trajectory_accvel_aretaon[:, 0].astype(int)
+actions_aretaon = trajectory_dirvel_aretaon[:, 1].astype(int)
+state_indices_dirvel_medauroidea = trajectory_dirvel_medauroidea[:, 0].astype(int)
+state_indices_accvel_medauroidea = trajectory_accvel_medauroidea[:, 0].astype(int)
+actions_medauroidea = trajectory_dirvel_medauroidea[:, 1].astype(int)
 
 
-q_values = q_value_ante[state_indices_ante]+q_value_dirvel[state_indices_dirvel]+q_value_accvel[state_indices_accvel]
+# q_values = q_value_ante[state_indices_ante]+q_value_dirvel[state_indices_dirvel]+q_value_accvel[state_indices_accvel]
 # q_values = (
 #     (q_value_ante[state_indices_ante] * (q_value_ante[state_indices_ante] > 0.5)) +
 #     (q_value_dirvel[state_indices_dirvel] * (q_value_dirvel[state_indices_dirvel] > 0.5)) +
 #     (q_value_accvel[state_indices_accvel] * (q_value_accvel[state_indices_accvel] > 0.5))
 # )
-print(q_values.shape)
+# print(q_values.shape)
+
+
+# the policy used
+q_values = q_value_dirvel_carausius
+# the test source
+actions = actions_aretaon
+
 replicated_trajectory = []
 for i in range(len(q_values)):
     action_probabilities = q_values[i]
